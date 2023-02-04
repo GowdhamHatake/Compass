8540932b0fc462a032a5aab1499ac253f4422a25
Gowdham <gowdhamandrewssingh@gmail.com>
2023-02-04 17:28:03 +0530

    First

--- src/test/java/ecom/boss/One.java
+++ src/test/java/ecom/boss/One.java
@@ -1,5 +1,25 @@
 package ecom.boss;
 
 public class One {
+	
+public static void main(String[] args) {
+	
+System.out.println(2);
+
+System.out.println(4);
+
+System.out.println(6);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
 
 }
